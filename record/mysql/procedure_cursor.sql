59cffa2060faccd5cacda5e1d1cc4097c5cf9b4b
lichuan <308831759@qq.com>
2012-02-25 16:56:44 +0800

    添加存储过程

--- record/mysql/procedure_cursor.sql
+++ record/mysql/procedure_cursor.sql
@@ -98,3 +98,133 @@ call update_pay();
 drop procedure update_pay;
 
 
+
+/*
+tbl_gold_get
+*/
+
+delimiter $$
+DROP PROCEDURE IF EXISTS pc_tbl_gold_get;
+create procedure pc_tbl_gold_get()
+BEGIN
+    DECLARE id1 INT(4);
+    DECLARE name1 varchar(50);    
+    DECLARE id2 INT(4);
+    DECLARE no_more INT(4);
+    DECLARE name2 char(50);    
+    DECLARE curs1 CURSOR FOR SELECT id, charname from tbl_gold_get;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND set no_more = 1;
+    OPEN curs1;
+    REPEAT
+        FETCH curs1 into id1, name1;
+        SELECT id into id2 from tbl_character_info where name = name1 limit 1;
+        update tbl_gold_get set charname = cast(id2 as char(50)) where id = id1;
+        UNTIL no_more = 1
+    END REPEAT;
+    close curs1;
+END $$
+delimiter ;
+UPDATE tbl_gold_get SET charname = REPLACE(REPLACE(charname, CHAR(10), ''), CHAR(13), '');
+call pc_tbl_gold_get();
+drop procedure pc_tbl_gold_get;
+
+ALTER TABLE tbl_gold_get CHANGE charname charid int(4) UNSIGNED DEFAULT '0';
+
+
+
+/*
+tbl_guild
+*/
+delimiter $$
+DROP PROCEDURE IF EXISTS pc_tbl_guild;
+create procedure pc_tbl_guild()
+BEGIN
+    DECLARE id1 INT(4);
+    DECLARE name1 char(50);    
+    DECLARE id2 INT(4);
+    DECLARE no_more INT(4);
+    DECLARE name2 char(50);
+    DECLARE name3 char(50);
+    DECLARE curs1 CURSOR FOR SELECT name, master from tbl_guild;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND set no_more = 1;
+    OPEN curs1;
+    REPEAT
+        FETCH curs1 into name1, name2;
+        SELECT id, guildname into id1, name3 from tbl_character_info where name = name2 limit 1;
+
+        if(name3 <> name1) THEN        
+            delete from tbl_guild_member where guild = name1;
+            delete from tbl_guild_request where guild = name1;
+            delete from tbl_guild where name = name1;
+        else
+            update tbl_guild set master = cast(id1 as char(50)) where name = name1;
+        end if;
+        UNTIL no_more = 1
+    END REPEAT;
+    close curs1;
+END $$
+delimiter ;
+UPDATE tbl_guild SET master = REPLACE(REPLACE(master, CHAR(10), ''), CHAR(13), '');
+call pc_tbl_guild();
+drop procedure pc_tbl_guild;
+
+update tbl_guild set master1 = '0', master2 = '0';
+ALTER TABLE tbl_guild CHANGE master master int(4) UNSIGNED DEFAULT '0';
+ALTER TABLE tbl_guild CHANGE master1 master1 int(4) UNSIGNED DEFAULT '0';
+ALTER TABLE tbl_guild CHANGE master2 master2 int(4) UNSIGNED DEFAULT '0';
+
+
+
+/*
+tbl_gold_member
+*/
+delimiter $$
+DROP PROCEDURE IF EXISTS pc_tbl_guild_member;
+create procedure pc_tbl_guild_member()
+BEGIN
+    DECLARE id1 INT(4);
+    DECLARE name1 char(50);    
+    DECLARE id2 INT(4);
+    DECLARE no_more INT(4);
+    DECLARE name2 char(50);
+    DECLARE name3 char(50);
+    DECLARE curs1 CURSOR FOR SELECT username from tbl_guild_member;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND set no_more = 1;
+    OPEN curs1;
+    REPEAT
+        FETCH curs1 into name1;
+        SELECT id into id1 from tbl_character_info where name = name1 limit 1;
+        update tbl_guild_member set username = cast(id1 as char(50)) where username = name1;
+        UNTIL no_more = 1
+    END REPEAT;
+    close curs1;
+END $$
+delimiter ;
+UPDATE tbl_guild_member SET username = REPLACE(REPLACE(username, CHAR(10), ''), CHAR(13), '');
+call pc_tbl_guild_member();
+drop procedure pc_tbl_guild_member;
+
+ALTER TABLE tbl_guild_member CHANGE username charid int(4) UNSIGNED DEFAULT '0';
+
+
+
+/*
+tbl_guild_request
+*/
+delete from tbl_guild_request;
+ALTER TABLE tbl_guild_request CHANGE requester charid int(4) UNSIGNED DEFAULT '0';
+ALTER TABLE tbl_log_level_up DROP COLUMN name;
+
+
+
+/*
+tbl_log_money
+*/
+
+delete from tbl_log_money;
+ALTER TABLE tbl_log_money CHANGE name charid int(4) UNSIGNED DEFAULT '0';
+update tbl_system_mailbox set fromname = '' WHERE mailfrom <> 0;
+ALTER TABLE tbl_system_mailbox DROP COLUMN toname;
+
+
+
